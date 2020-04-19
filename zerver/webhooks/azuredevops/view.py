9a9a5acbb7e3c49fd47380e9f153d3f83eb4b78a
Aryan Ebrahimpour <oxaryan@outlook.com>
2020-04-19 12:57:06 +0430

    get topic from url

--- zerver/webhooks/azuredevops/view.py
+++ zerver/webhooks/azuredevops/view.py
@@ -10,11 +10,12 @@ from zerver.lib.response import json_error, json_success
 from zerver.lib.validator import check_dict, check_string
 from zerver.models import UserProfile
 
-@api_key_only_webhook_view('HelloWorld')
+@api_key_only_webhook_view('AzureDevops')
 @has_request_variables
-def api_helloworld_webhook(
+def api_azuredevops_webhook(
         request: HttpRequest, user_profile: UserProfile,
-        payload: Dict[str, Iterable[Dict[str, Any]]]=REQ(argument_type='body')
+        payload: Dict[str, Iterable[Dict[str, Any]]]=REQ(argument_type='body'),
+        topic: str=REQ(default='coverage')
 ) -> HttpResponse:
 
     # construct the body of the message
@@ -24,7 +25,6 @@ def api_helloworld_webhook(
     body_template = '\n{detailedMessage.markdown}'
     body += body_template.format(**payload)
 
-    topic = "Hello World"
 
     # send the message
     check_send_webhook_message(request, user_profile, topic, body)
